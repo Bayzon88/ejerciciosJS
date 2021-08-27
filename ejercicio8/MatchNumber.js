1a9b32ebb6e5462d27cba00d3a96c70f55f0e062
Bayzon88 <beltranalvaro08@gmail.com>
2021-08-26 22:27:46 -0500

    ejercicios con redondear numeros y random

---
+++ ejercicio8/MatchNumber.js
@@ -0,0 +1,12 @@
+let userNumber = prompt("ingrese un numero entre 1 y 10");
+
+let randomNumer = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
+if (userNumber == randomNumer(1, 2)) {
+  alert("ganaste");
+} else {
+  alert("perdiste");
+}
+
+console.log(Math.round(randomNumer(1, 2), 0));
