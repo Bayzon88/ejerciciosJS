e4463c5b2e348b602320e8b888ca54d17fe592ce
Alvaro Ricardo Beltran Cáceres <ABELTRAN@upc.edu.pe>
2021-08-31 22:18:47 -0500

    ejercicios

---
+++ BasicJS_150/Ejercicio31/LargestOf3.js
@@ -0,0 +1,5 @@
+let largestOf3 = (num1, num2, num3) => {
+  return Math.max(num1, num2, num3);
+};
+
+console.log(largestOf3(3, 6, 4));
---
+++ BasicJS_150/Ejercicio32/NearestTo100.js
@@ -0,0 +1,5 @@
+let nearestTo100 = (num1, num2) => {
+  return Math.max(num1, num2);
+};
+
+console.log(nearestTo100(99, 35));
